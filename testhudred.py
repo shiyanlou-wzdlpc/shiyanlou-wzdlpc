56a048ba8ef924e5206c978db6ab86013decb413
shiyanlou-wzdlpc <wzdlpc@qq.com>
2018-04-05 23:53:31 +0800

    Python3 test file

---
+++ testhudred.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+number = int(input("Enter an integer: "))
+if number <= 100:
+    print("Your number is smaller than equal to 100")
+else:
+    print("Your number is greater than 100")
