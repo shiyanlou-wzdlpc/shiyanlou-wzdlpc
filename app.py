ebd6a39f4351df1ee344b0155c7384fa7b658c09
shiyanlou-wzdlpc <wzdlpc@qq.com>
2018-05-31 07:05:27 +0800

    2018.05.31

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -10,13 +10,23 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 def index():
     path = '/home/shiyanlou/files'
     file_titles_list = []
-    for filename in os.listdir(path):
-        with open(filename) as f:
+    for jsonfile in os.listdir(path):
+        with open(jsonfile) as f:
             for k, y in json.loads(f.read()).items():
                 if  k == 'title':
                     file_titles_list.append(y)
     titles = {'title_list': file_titles_list}
     return render_template('index.html', titles=titles)
+
+@app.route('/files/<filename>')
+def file(filename):
+    path = '/home/shiyanlou/files/' + filename
+    if filename in os.listdir(path):
+        return redirect(url_for('404.html'))
+    else:
+        with open(path) as f:
+            readfile = json.loads(f.read())
+        return render_template('file.html',readfile=readfile)
 
 
 
