5fac0c650fa8f7eb60ba37b83e7f66cd6ef8022d
shiyanlou-wzdlpc <wzdlpc@qq.com>
2018-06-10 16:29:41 +0800

    从数据库中读取内容

--- app3.py
+++ app3.py
@@ -1,20 +1,20 @@
 #!usr/bin/env python3
 from flask import Flask, render_template, url_for
 from datetime import datetime
-from flask_slqalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.update['TEMPLATES_AUTO_RELOAD'] = True
-app.config.update['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/wzdlpc'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/wzdlpc'
 db = SQLAlchemy(app)
 
-class File(db.model):
+class File(db.Model):
     __tablename__ = 'files'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     created_time = db.Column(db.DateTime)
-    category_id = db.Column(db.Integer, db.ForeignKey(category.id))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
     category = db.relationship('Category')
     content = db.Column(db.Text)
 
@@ -27,8 +27,8 @@ class File(db.model):
     def __repr__(self):
         return '<File(name=%s)>' % self.title
 
-class Category(db.model):
-    __tablename__ = 'categiries'
+class Category(db.Model):
+    __tablename__ = 'categories'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
@@ -39,17 +39,32 @@ class Category(db.model):
     def __repr__(self):
         return '<Category(name=%s)>' % self.name
 
+def insert_data():
+    java =Category('Java')
+    python = Category('Python')
+    file1 = File('Hello Java', datetime.utcnow(), java, 'File Content - Java is cool!')
+    file2 = File('Hello Python', datetime.utcnow(), python, 'File Content - Python is cool!')
+    db.session.add(java)
+    db.session.add(python)
+    db.session.add(file1)
+    db.session.add(file2)
+    db.session.commit()
+
+
 @app.route('/')
 def index():
-    pass
+    files = db.session.query(File).all()
+    
+    return render_template('index.html',files=files)
 
 @app.route('/files/<int:file_id>')
 def file(file_id):
-    pass
+    file_item = db.session.query(File).get_or_404(file_id)
+    return render_template('file.html', file_item=file_item)
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_tamplate('404.html', 404)
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run()
